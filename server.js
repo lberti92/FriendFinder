d3a18d3ba2656962030b0892ef3bd65aef0a1fb5
Breeze <lberti@msn.com>
2019-07-23 18:12:54 -0600

    unsure of changes doing a commit

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require("express");
 
 var app = express();
-
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require("./app/routing/apiRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
