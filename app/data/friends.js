8d4b5aa0d6e62abbe30ec3afb43931031eeb4e9e
Breeze <lberti@msn.com>
2019-08-19 16:36:09 -0600

    added apis with images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,69 +1,149 @@
 
-  var friendsArray = [
-{
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+var friendsArray = [
+  {
+    name: "Tigger",
+    photo: "http://img1.wikia.nocookie.net/__cb20130128233705/disney/images/6/67/Tigger_KHII.png",
     scores: [
-    "5",
-    "1",
-    "4",
-    "4",
-    "5",
-    "1",
-    "2",
-    "5",
-    "4",
-    "1"
+      "4",
+      "4",
+      "1",
+      "2",
+      "5",
+      "3",
+      "5",
+      "1",
+      "3",
+      "5"
     ]
-    },
-    {
-    name: "Jacob Deming",
-    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+  },
+  {
+    name: "Donald Duck",
+    photo: "https://tse1.mm.bing.net/th?id=OIP.Xqk7bOZByR1u1ZEENLDBIAHaKL&pid=Api&P=0&w=300&h=300",
     scores: [
-    "4",
-    "2",
-    "5",
-    "1",
-    "3",
-    "2",
-    "2",
-    "1",
-    "3",
-    "2"
+      "5",
+      "3",
+      "2",
+      "2",
+      "3",
+      "3",
+      "3",
+      "1",
+      "3",
+      "5"
     ]
-    },
-    {
-    name: "Jeremiah Scanlon",
-    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+  },
+  {
+    name: "Merida",
+    photo: "https://1.bp.blogspot.com/-zyx1twjxMZ4/U4iqkcYCiVI/AAAAAAAANdM/vZmCEeRA9co/s1600/Pixar+Post+-+Merida+Brave+Disney+Infinity+2+Figure.jpg",
     scores: [
+      "5",
+      "2",
+      "4",
+      "2",
+      "1",
+      "3",
+      "5",
+      "1",
+      "3",
+      "5"
+    ]
+  },
+  {
+    name: "Dory",
+    photo: "https://larryfire.files.wordpress.com/2016/03/finding-dory-fdcs_dory10_125-per16-125.jpg",
+    scores: [
+      "5",
+      "1",
+      "5",
+      "1",
+      "5",
+      "1",
+      "3",
+      "1",
+      "3",
+      "5"
+    ]
+  },
+  {
+    name: "Olaf",
+    photo: "https://i.pinimg.com/736x/3f/b0/bb/3fb0bb68c81d0031c1d7a2926304dc86.jpg",
+    scores: [
+      "4",
+      "1",
+      "3",
+      "1",
+      "5",
+      "4",
+      "5",
+      "2",
+      "4",
+      "5"
+    ]
+  },
+  {
+    name: "Grumpy",
+    photo: "https://ohmy.disney.com/wp-content/uploads/2014/08/Disney-Characters-Grumpier-Than-Grumpy.png",
+    scores: [
+      "1",
+      "1",
+      "1",
+      "5",
+      "3",
+      "5",
+      "1",
+      "5",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: "Jimmy Cricket",
+    photo: "https://tse1.mm.bing.net/th?id=OIP.wJgi_vGfVVO_40oCJqt5qAHaFh&pid=Api&P=0&w=241&h=181",
+    scores: [
+      "5",
+      "4",
+      "5",
+      "4",
+      "5",
+      "1",
+      "4",
+      "3",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Mickey Mouse",
+    photo: "https://upload.wikimedia.org/wikipedia/en/d/d4/Mickey_Mouse.png",
+    scores: [
+      "5",
+      "5",
+      "5",
+      "1",
+      "5",
+      "1",
+      "1",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+  name: "Winnie the Pooh",
+  photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiSuaOk6o_kAhVUHzQIHT8gATQQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.walmart.com%2Fip%2FWinnie-the-Pooh-Cardboard-Stand-Up-3ft%2F24873609&psig=AOvVaw2BcWGzMeIbGgmtYbCpMOko&ust=1566334881781264https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fc7%2F2c%2Fa6%2Fc72ca6b569e9729191b465dba7dda209.png&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F329396160230484066%2F&docid=gDTGWwmFTtYAPM&tbnid=-IQ0bDY_X-CkNM%3A&vet=10ahUKEwjwj_mi6o_kAhWqUt8KHQDNCmkQMwiEASgGMAY..i&w=492&h=900&bih=726&biw=1344&q=winnie%20the%20pooh&ved=0ahUKEwjwj_mi6o_kAhWqUt8KHQDNCmkQMwiEASgGMAY&iact=mrc&uact=8https://i2.wp.com/betweenusparents.com/wp-content/uploads/2017/01/Pooh-bear-clip-art-winniepooh_1_800_800.jpg",
+  scores: [
     "5",
-    "2",
-    "2",
-    "2",
-    "4",
     "1",
     "3",
+    "1",
+    "4",
     "2",
-    "5",
+    "1",
+    "1",
+    "3",
     "5"
-    ]
-    },
-    {
-    name: "Louis T. Delia",
-    photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-    scores: [
-    "3",
-    "3",
-    "4",
-    "2",
-    "2",
-    "1",
-    "3",
-    "2",
-    "2",
-    "3"
-    ]
-    }
+  ]
+}
 ];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,25 +1,44 @@
 var friends = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res) {
-    
-      res.json(response);
-  })
 
-  
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
+    var friendArray = req.body
+    var scoresArray = req.body.scores;
+    var compatibilityScore = 41;
+    var bestMatch = friends[0];
+    
+    
+    for (var i = 0; i < friends.length; i++) {
+      var currentFriend = friends[i];
+      var difference = 0;
+      console.log(currentFriend);
+      
+      for (var j = 0; j < scoresArray.length; j++) {
+        var friendScore = scoresArray[j];
+        difference += Math.abs(friends[i].scores[j] - scoresArray[j]);
+        console.log("FriendScore" + friendScore);
+        console.log(difference);
+      
+      }
 
-    var friendsArray = req.body;
-    
-    console.log(friendsArray);
-  
-    friends.push(friendsArray);
-    
-    res.json(friendsArray);
+      if (difference <= compatibilityScore){
+        bestMatch = friends[i];
+        compatibilityScore = difference;
+        ///update the compatibility Score
+      }
+
+      // whoever has the smaller number based on our input (we are computer)
+      // save the 1st friendArray value and then go to the next friend if the next frient
+      // is smaller than or put new value in VAR and so on until looped through all friends
+      };
+
+    res.json(bestMatch);
+    friends.push(req.body);
   });
 };
